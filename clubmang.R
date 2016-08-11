467268c977b534b7c497571df2fc73758d3df4d7
Sungpil Han CPT <shan@acp.kr>
2016-08-11 16:37:27 +0900

    코드업뎃, 아웃풋

--- clubmang.R
+++ clubmang.R
@@ -1,8 +1,10 @@
 몽액육즙=c("육","몽","액","즙")
 Mong = apply(expand.grid(몽액육즙,몽액육즙,몽액육즙,몽액육즙), 1, paste, collapse = '') 
-write.table(Mong, "Mong.txt")
+write.table(Mong, "Mong.txt", row.names = FALSE, col.names = FALSE, eol = " ")
 
 조합 = c("잉","뽀","망","몽")
 잉뽀망몽=expand.grid(조합,조합,조합,조합)
 코=data.frame(잉뽀망몽$Var1, "월 ", 잉뽀망몽$Var2, "일 ", 잉뽀망몽$Var3, "시 ", 잉뽀망몽$Var4, "분은?") 
-apply(코, 1, paste, collapse = '') 
+IngPpo = apply(코, 1, paste, collapse = '') 
+write.table(IngPpo, "IngPpo.txt", row.names = FALSE, col.names = FALSE, eol = " ")
+
